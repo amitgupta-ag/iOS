e977d620d4225c2fc6d9cf20eddb2c029298913e
Amit Gupta <amitgupta.nitb [at] gmail [dot] com>
2017-05-03 20:35:25 +0200

    alignment constraints set

--- emoticoins/emoticoins/ViewController.swift
+++ emoticoins/emoticoins/ViewController.swift
@@ -13,6 +13,17 @@ class ViewController: UIViewController {
     
     @IBOutlet var labelStatus: UILabel!
     
+    @IBOutlet var labelHitCount: UILabel!
+    
+    @IBOutlet var imageContainer: UIImageView!
+    
+    
+    var hitcountHappy = 0
+    var hitcountCrazy = 0
+    var hitcountSad = 0
+    var hitcountAngry = 0
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -26,28 +37,33 @@ class ViewController: UIViewController {
     
     @IBAction func buttonHappy(_ sender: UIButton) {
         labelStatus.text = "Happy Baby!"
-        //resetTheLabel()
+        hitcountHappy = hitcountHappy+1
+        resetHitsReceived(hitcountHappy)
     }
     
     @IBAction func buttonCrazy(_ sender: Any) {
         labelStatus.text = "Feeling Crazy!"
-        //resetTheLabel()
+        hitcountCrazy = hitcountCrazy+1
+        resetHitsReceived(hitcountCrazy)
     }
     
     @IBAction func buttonSad(_ sender: Any) {
         labelStatus.text = "Feeling sad!"
-        //resetTheLabel()
+        hitcountSad = hitcountSad+1
+        resetHitsReceived(hitcountSad)
     }
     
     @IBAction func buttonAngry(_ sender: Any) {
         labelStatus.text = "Feeling Angry!"
-        //resetTheLabel()
+        hitcountAngry = hitcountAngry+1
+        resetHitsReceived(hitcountAngry)
     }
     
-    func resetTheLabel(){
-        sleep(1000)
-        //labelStatus.text=""
-    
+    func resetHitsReceived(_ hits: Int){
+        labelHitCount.text = String(hits)
+        labelStatus.textColor = #colorLiteral(red: 0.1019607857, green: 0.2784313858, blue: 0.400000006, alpha: 1)
+        labelStatus.font = labelStatus.font.withSize(CGFloat(hits < 4 ? 20 : (hits > 8 ? 40 : 3*hits+15)))
+        
     }
     
 }
