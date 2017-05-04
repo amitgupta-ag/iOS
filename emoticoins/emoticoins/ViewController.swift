17e3f2ce0b4e8b1a171523149dcd3e5ab42779e3
Amit Gupta <amitgupta.nitb [at] gmail [dot] com>
2017-05-04 12:49:58 +0200

    using gif rendered UIBox. feeling status updates based on the hitScores

--- emoticoins/emoticoins/ViewController.swift
+++ emoticoins/emoticoins/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
     
     @IBOutlet var imageContainer: UIImageView!
     
+    @IBOutlet var imageGif: UIImageView!
     
     var hitcountHappy = 0
     var hitcountCrazy = 0
@@ -38,32 +39,51 @@ class ViewController: UIViewController {
     @IBAction func buttonHappy(_ sender: UIButton) {
         labelStatus.text = "Happy Baby!"
         hitcountHappy = hitcountHappy+1
-        resetHitsReceived(hitcountHappy)
+        resetHitsReceived(hitcountHappy, gifname: "gifHappy", feeling: "happy")
     }
     
     @IBAction func buttonCrazy(_ sender: Any) {
         labelStatus.text = "Feeling Crazy!"
         hitcountCrazy = hitcountCrazy+1
-        resetHitsReceived(hitcountCrazy)
+        resetHitsReceived(hitcountCrazy, gifname: "gifCrazy", feeling: "crazy")
     }
     
     @IBAction func buttonSad(_ sender: Any) {
         labelStatus.text = "Feeling sad!"
         hitcountSad = hitcountSad+1
-        resetHitsReceived(hitcountSad)
+        resetHitsReceived(hitcountSad, gifname: "gifSad", feeling: "sad")
     }
     
     @IBAction func buttonAngry(_ sender: Any) {
         labelStatus.text = "Feeling Angry!"
         hitcountAngry = hitcountAngry+1
-        resetHitsReceived(hitcountAngry)
+        resetHitsReceived(hitcountAngry, gifname: "gifAngry", feeling: "angry")
     }
     
-    func resetHitsReceived(_ hits: Int){
+    func resetHitsReceived(_ hits: Int, gifname: String, feeling: String){
+        labelStatus.isHidden = false
+        imageGif.isHidden = true
         labelHitCount.text = String(hits)
         labelStatus.textColor = #colorLiteral(red: 0.1019607857, green: 0.2784313858, blue: 0.400000006, alpha: 1)
         labelStatus.font = labelStatus.font.withSize(CGFloat(hits < 4 ? 20 : (hits > 8 ? 40 : 3*hits+15)))
-        
+        if (hits > 9 && hits <= 14){
+            switch feeling {
+            case "happy":
+                labelStatus.text = "you gotcha\' party! \\m/"
+            case "crazy":
+                labelStatus.text = "You\'re wild! :D"
+            case "sad":
+                labelStatus.text = "O\' you poor thing.."
+            case "angry":
+                labelStatus.text = "O\' grumpy baby.."
+            default:
+                labelStatus.text = "..even more?"
+            }
+        }else if(hits > 14){
+            labelStatus.isHidden = true
+            imageGif.loadGif(name: gifname)
+            imageGif.isHidden = false
+        }
     }
     
 }
