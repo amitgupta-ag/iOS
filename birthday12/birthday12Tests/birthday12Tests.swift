89ced71b8ca503f3bd5a7d86c51e53c7359a1f04
Amit Gupta <amitgupta.nitb [at] gmail [dot] com>
2017-05-05 13:22:09 +0200

    basecode - first commit

---
+++ birthday12/birthday12Tests/birthday12Tests.swift
@@ -0,0 +1,36 @@
+//
+//  birthday12Tests.swift
+//  birthday12Tests
+//
+//  Created by Amit Gupta on 5/5/17.
+//  Copyright © 2017 AGx. All rights reserved.
+//
+
+import XCTest
+@testable import birthday12
+
+class birthday12Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
